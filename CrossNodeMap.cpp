c23a7dcca379ee760c1bb950e82762613bc1ad57
yhx <shen_yhx@163.com>
2021-05-14 07:43:00 +0800

    update cnm

--- CrossNodeMap.cpp
+++ CrossNodeMap.cpp
@@ -5,27 +5,33 @@
 #include "../utils/utils.h"
 #include "CrossNodeMap.h"
 
-int sendMap(CrossNodeMap *map_, int dest, int tag, MPI_Comm comm) 
+int CrossNodeMap::send(int dest, int tag, MPI_Comm comm) 
 {
 	int ret = 0;
-	ret = MPI_Send(map_, sizeof(CrossNodeMap), MPI_UNSIGNED_CHAR, dest, tag, comm);
+	ret = MPI_Send(_num, 1, MPI_INT, dest, tag, comm);
 	assert(ret == MPI_SUCCESS);
-	ret = MPI_Send(map_->_idx2index, map_->_num, MPI_INT, dest, tag+1, comm);
+	ret = MPI_Send(_cross_size, 1, MPI_INT, dest, tag, comm);
 	assert(ret == MPI_SUCCESS);
-	ret = MPI_Send(map_->_crossnodeIndex2idx, map_->_crossSize, MPI_INT, dest, tag+2, comm);
+	ret = MPI_Send(_idx2index, _num, MPI_INT, dest, tag+1, comm);
+	assert(ret == MPI_SUCCESS);
+	ret = MPI_Send(_crossnodeIndex2idx, _cross_size, MPI_INT, dest, tag+2, comm);
 	assert(ret == MPI_SUCCESS);
 	return ret;
 }
 
-CrossNodeMap * recvMap(int src, int tag, MPI_Comm comm)
+int CrossNodeMap::recv(int src, int tag, MPI_Comm comm)
 {
-	CrossNodeMap *net = (CrossNodeMap *)malloc(sizeof(CrossNodeMap));
 	int ret = 0;
 	MPI_Status status;
-	ret = MPI_Recv(net, sizeof(CrossNodeMap), MPI_UNSIGNED_CHAR, src, tag, comm, &status);
+	ret = MPI_Recv(_num, 1, MPI_INT, src, tag, comm, &status);
 	assert(ret==MPI_SUCCESS);
-	net->_idx2index = (int *)malloc(sizeof(int)*net->_num);
-	ret = MPI_Recv(net->_idx2index, net->_num, MPI_INT, src, tag+1, comm, &status);
+	ret = MPI_Recv(_cross_size, 1, MPI_INT, src, tag, comm, &status);
+	assert(ret==MPI_SUCCESS);
+	if (_idx2index) {
+		free(_idx2index);
+	}
+	_idx2index = (int *)malloc(sizeof(int)*net->_num);
+	ret = MPI_Recv(_idx2index, net->_num, MPI_INT, src, tag+1, comm, &status);
 	assert(ret==MPI_SUCCESS);
 	net->_crossnodeIndex2idx = (int *)malloc(sizeof(int)*(net->_crossSize));
 	ret = MPI_Recv(net->_crossnodeIndex2idx, net->_crossSize, MPI_INT, src, tag+2, comm, &status);
--- CrossNodeMap.h
+++ CrossNodeMap.h
@@ -4,7 +4,7 @@
 
 #include "mpi.h"
 
-struct CrossNodeMap {
+class CrossNodeMap {
 	// ID of neurons on this node to index in this map 
 	// index = _idx2index[id]
 	int *_idx2index;
@@ -12,17 +12,23 @@ struct CrossNodeMap {
 	// id = _crossnode_index2idx[index * node_num + j], -1 means no shadow neuron on node j
 	int *_crossnodeIndex2idx;
 	// _cross_size = node_num * number_of_the_neurons_on_this_node_which_have_crossnode_connections
-	int _crossSize;
+	int _cross_size;
 	int _num;
+	
+	int save(FILE *f);
+	int load(FILE *f);
+	int compare(CrossNodeMap &m);
+	int send(int dest, int tag, MPI_Comm comm);
+	int recv(int src, int tag, MPI_Comm comm);
 };
 
 
-int saveCNM(CrossNodeMap *map, FILE *f);
-CrossNodeMap *loadCNM(FILE *f); 
-int compareCNM(CrossNodeMap *m1, CrossNodeMap *m2);
-
-int sendMap(CrossNodeMap * network, int dest, int tag, MPI_Comm comm);
-CrossNodeMap * recvMap(int src, int tag, MPI_Comm comm);
+// int saveCNM(CrossNodeMap *map, FILE *f);
+// CrossNodeMap *loadCNM(FILE *f); 
+// int compareCNM(CrossNodeMap *m1, CrossNodeMap *m2);
+// 
+// int sendMap(CrossNodeMap * network, int dest, int tag, MPI_Comm comm);
+// CrossNodeMap * recvMap(int src, int tag, MPI_Comm comm);
 
 
 #endif // CROSSNODEMAP_H
