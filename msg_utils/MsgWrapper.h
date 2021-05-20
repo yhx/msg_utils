77f119dfc84faae94d4937abe57e8315ccc67c5c
yhx <shen_yhx@163.com>
2021-05-21 01:49:26 +0800

    update MsgWrapper.h

--- msg_utils/MsgWrapper.h
+++ msg_utils/MsgWrapper.h
@@ -18,8 +18,14 @@ public:
 	MsgWrapper();
 	~MsgWrapper();
 
+	int send_meta(int dst, int tag);
+	int recv_meta(int dst, int tag);
+
 	int send_meta(int dst, int tag, MPI_Comm comm);
 	int recv_meta(int dst, int tag, MPI_Comm comm);
+
+	int send(int dst, int tag);
+	int recv(int src, int tag);
 
 	int send(int dst, int tag, MPI_Comm comm);
 	int recv(int src, int tag, MPI_Comm comm);
@@ -44,7 +50,11 @@ template<typename T>
 MsgWrapper<T>::MsgWrapper(size_t num)
 {
 	_num = num;
-	_data = new T[num];
+	if (num > 0) {
+		_data = new T[num];
+	} else {
+		_data = NULL;
+	}
 }
 
 template<typename T>
@@ -55,12 +65,24 @@ MsgWrapper<T>::~MsgWrapper()
 }
 
 template<typename T>
+int MsgWrapper<T>::send_meta(int dst, int tag)
+{
+	return send_meta(dst, tag, MPI_COMM_WORLD);
+}
+
+template<typename T>
 int MsgWrapper<T>::send_meta(int dst, int tag, MPI_Comm comm)
 {
 	int ret = 0;
 	ret = MPI_Send(&_num, 1, MPI_SIZE_T, dst, tag, comm);
 	assert(ret == MPI_SUCCESS);
 	return 1;
+}
+
+template<typename T>
+int MsgWrapper<T>::recv_meta(int src, int tag)
+{
+	return recv_meta(src, tag, MPI_COMM_WORLD);
 }
 
 template<typename T>
@@ -80,12 +102,24 @@ int MsgWrapper<T>::recv_meta(int src, int tag, MPI_Comm comm)
 }
 
 template<typename T>
+int MsgWrapper<T>::send(int dst, int tag)
+{
+	return send(dst, tag, MPI_COMM_WORLD);
+}
+
+template<typename T>
 int MsgWrapper<T>::send(int dst, int tag, MPI_Comm comm)
 {
 	int ret = 0;
-	ret = MPI_Send(&_data, sizeof(T) * _num, MPI_UNSIGNED_CHAR, dst, tag, comm);
+	ret = MPI_Send(_data, sizeof(T) * _num, MPI_UNSIGNED_CHAR, dst, tag, comm);
 	assert(ret == MPI_SUCCESS);
 	return 1;
+}
+
+template<typename T>
+int MsgWrapper<T>::recv(int src, int tag)
+{
+	return recv(src, tag, MPI_COMM_WORLD);
 }
 
 template<typename T>
@@ -93,7 +127,7 @@ int MsgWrapper<T>::recv(int src, int tag, MPI_Comm comm)
 {
 	int ret = 0;
 	MPI_Status status;
-	ret = MPI_Recv(&_data, sizeof(T) * _num, MPI_UNSIGNED_CHAR, src, tag, comm, &status);
+	ret = MPI_Recv(_data, sizeof(T) * _num, MPI_UNSIGNED_CHAR, src, tag, comm, &status);
 	assert(ret == MPI_SUCCESS);
 	return ret;
 }
@@ -107,6 +141,7 @@ T * MsgWrapper<T>::get_value(size_t idx)
 template<typename T>
 void MsgWrapper<T>::print()
 {
+	cout << _num << endl;
 	for (size_t i=0; i<_num; i++) {
 		cout << _data[i] << " ";
 	}
